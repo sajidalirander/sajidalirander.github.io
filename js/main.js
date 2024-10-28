5629df9765a65b33b361826345030fc7c8f2ddf4
SajidDeltax <sajidali@deltax.ai>
2024-10-28 19:04:25 +0900

    updated index

--- js/main.js
+++ js/main.js
@@ -19,9 +19,9 @@
 
 	var fullHeight = function() {
 
-		$('.js-fullheight').css('height', $(window).height() / 40);
+		$('.js-fullheight').css('height', $(window).height() / 10);
 		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height() / 40);
+			$('.js-fullheight').css('height', $(window).height() / 10);
 		});
 
 	};
